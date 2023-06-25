a4446fc43c4fa682a0f908128a93bd9f5cb49b84
Fillipe143 <fillipedev.contato@gmail.com>
2023-06-25 14:40:00 -0300

    added getActivationInstanceOf tests

---
+++ test/activation.spec.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "vitest";
+import { ActivationFunctionType, Sigmoid } from "../src";
+import {getActivationInstanceOf } from "../src/utils";
+
+test("is returning activation class instance correctly", () => {
+    const type: ActivationFunctionType = "sigmoid";
+    const activation = getActivationInstanceOf(type);
+
+    expect(activation).instanceOf(Sigmoid);
+});
