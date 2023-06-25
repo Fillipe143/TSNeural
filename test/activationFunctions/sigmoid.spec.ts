2acd0e989935deb09c2775422bc7a4d3edff1cde
Fillipe143 <fillipedev.contato@gmail.com>
2023-06-24 23:51:36 -0300

    standardization of activation function tests

--- test/activationFunctions/sigmoid.spec.ts
+++ test/activationFunctions/sigmoid.spec.ts
@@ -2,10 +2,10 @@ import { expect, test } from "vitest";
 import { Sigmoid } from "../../src";
 
 test("is activating number using sigmoid correctly", () => {
-    // Sigmoid(0) = 1/2;
+    // Sigmoid(0) = 0.5;
 
     const sigmoid = new Sigmoid();
+    
     const activateInput = sigmoid.activate(0);
-
-    expect(activateInput).equals(1 / 2);
+    expect(activateInput).equals(0.5);
 });
--- test/activationFunctions/tanh.spec.ts
+++ test/activationFunctions/tanh.spec.ts
@@ -5,7 +5,7 @@ test("is activating number using tanh correctly", () => {
     // Tanh(0) = 0;
 
     const tanh = new Tanh();
+    
     const activateInput = tanh.activate(0);
-
     expect(activateInput).equals(0);
 });
