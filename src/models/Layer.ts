39127d6ce8c63059d3d8ece5a6a65d7672cb474c
Fillipe143 <fillipedev.contato@gmail.com>
2023-06-25 15:18:16 -0300

    added calculateOutputs in Layer

--- src/models/Layer.ts
+++ src/models/Layer.ts
@@ -1,6 +1,6 @@
-import { InvalidPositiveIntegerError } from "../errors";
-import { LayerProperties } from "../types";
-import { gaussianRandom } from "../utils";
+import { InvalidInputSizeError, InvalidPositiveIntegerError } from "../errors";
+import { ActivationFunctionType, LayerProperties } from "../types";
+import { gaussianRandom, getActivationInstanceOf } from "../utils";
 
 export class Layer {
     private numNodesIn: number;
@@ -22,12 +22,38 @@ export class Layer {
         if (numNodesIn <= 0 || numNodesOut <= 0 || !Number.isInteger(numNodesIn) || !Number.isInteger(numNodesOut)) {
             throw new InvalidPositiveIntegerError();
         }
-        
+
         this.numNodesIn = numNodesIn;
         this.numNodesOut = numNodesOut;
 
         this.weights = this.createArrayWithRandomValues(numNodesIn * numNodesOut);
         this.biases = this.createArrayWithRandomValues(numNodesOut);
+    }
+
+    public calculateOutputs(inputs: number[], activationType: ActivationFunctionType): number[] {
+        if (inputs.length !== this.numNodesIn) {
+            throw new InvalidInputSizeError();
+        }
+
+        const { activate } = getActivationInstanceOf(activationType);
+        const outputs: number[] = new Array(this.numNodesOut);
+
+        for (let nodeOut = 0; nodeOut < this.numNodesOut; nodeOut++) {
+
+            let weightedInput = this.biases[nodeOut];
+            for (let nodeIn = 0; nodeIn < this.numNodesIn; nodeIn++) {
+                weightedInput += inputs[nodeIn] * this.weights[this.getWeightFlatIndex(nodeIn, nodeOut)];
+            }
+
+            outputs[nodeOut] = activate(weightedInput);
+        }
+
+        return outputs;
+    }
+
+    private getWeightFlatIndex(nodeIn: number, nodeOut: number): number {
+        const flatIndex = nodeOut * this.numNodesIn + nodeIn;
+        return flatIndex;
     }
 
     private createArrayWithRandomValues(length: number): number[] {
