8ef4c2e1fa7941f8fad0b03f2deefc0b248a554d
Fillipe143 <fillipedev.contato@gmail.com>
2023-06-25 17:01:18 -0300

    added loadProps in NeuralNetwork

--- src/models/NeuralNetwork.ts
+++ src/models/NeuralNetwork.ts
@@ -1,5 +1,6 @@
-import { LayerSizeInsufficientError } from "../errors";
+import { InvalidNumberOfNodesError, LayerSizeInsufficientError } from "../errors";
 import { ActivationFunction, NeuralProperties } from "../types";
+import { getActivationInstanceOf } from "../utils";
 import { Layer } from "./Layer";
 import { Sigmoid } from "./activationFunctions/Sigmoid";
 
@@ -24,5 +25,28 @@ export class NeuralNetwork {
         });
 
         this.activationFunc = activationFunc;
+    }
+
+    public loadProps(props: NeuralProperties): void {
+        const { layers, activation } = props;
+
+        if (layers.length < 1) {
+            throw new LayerSizeInsufficientError("The minimum amount of layer sizes is 1.");
+        }
+
+        const nLayers: Layer[] = new Array(layers.length);
+
+        for (let i = 0; i < nLayers.length; i++) {
+            if (i === nLayers.length - 1 || layers[i].numNodesOut == layers[i + 1].numNodesIn) {
+                const { numNodesIn, numNodesOut } = layers[i];
+                nLayers[i] = new Layer(numNodesIn, numNodesOut);
+                nLayers[i].loadProps(layers[i]);
+            } else {
+                throw new InvalidNumberOfNodesError();
+            }
+        }
+
+        this.layers = nLayers;
+        this.activationFunc = getActivationInstanceOf(activation);
     }
 }
