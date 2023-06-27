b4ca477b14184413f5570224c5cda99cef74ae07
Fillipe143 <fillipedev.contato@gmail.com>
2023-06-27 09:16:27 -0300

    added calculateNodeCost tests

---
+++ test/nodeCost.spec.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "vitest";
+import { calculateNodeCost } from "../src/utils";
+
+test("should be return a positive integer", () => {
+    const output = 0;
+    const expectOutput = 1;
+
+    const cost = calculateNodeCost(output, expectOutput);
+    expect(!isNaN(cost) && cost >= 0).toEqual(true);
+});
